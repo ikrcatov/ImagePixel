a2b007dc98911ce04850da7f74a5287a1d5bf818
ikrcatov <acschnitzer@gmx.com>
2014-11-28 18:15:11 +0100

    Added All Files to REPO

---
+++ ImagePixel/ImagePixel/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ImagePixel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open First Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open Second Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 368);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(488, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(488, 368);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 437);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ ImagePixel/ImagePixel/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace ImagePixel
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> listPB;
+        Point myPoint = new Point(-1, -1);
+
+        public Form1()
+        {
+            InitializeComponent();
+            listPB = new List<PictureBox>();
+            listPB.Add(pictureBox1);
+            listPB.Add(pictureBox2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Loop through the picture boxes
+                foreach (PictureBox pb in listPB)
+                {
+                    //Find an empty picture box
+                    if (pb.Image == null)
+                    {
+                        //Load the image
+                        pb.Load(openFileDialog1.FileName);
+                        Image img = pb.Image;
+                        //Adjust the image size after loading it to Picture box
+                        if (pb.Width < img.Width && pb.Height < img.Height)
+                        {
+                            pb.SizeMode = PictureBoxSizeMode.Zoom;
+                        }
+                        else
+                        {
+
+                            pb.SizeMode = PictureBoxSizeMode.Normal;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Loop through the picture boxes
+                foreach (PictureBox pb in listPB)
+                {
+                    //Find an empty picture box
+                    if (pb.Image == null)
+                    {
+                        //Load the image
+                        pb.Load(openFileDialog1.FileName);
+                        Image img = pb.Image;
+                        //Adjust the image size after loading it to Picture box
+                        if (pb.Width < img.Width && pb.Height < img.Height)
+                        {
+                            pb.SizeMode = PictureBoxSizeMode.Zoom;
+                        }
+                        else
+                        {
+
+                            pb.SizeMode = PictureBoxSizeMode.Normal;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (myPoint.X != -1 || myPoint.Y != -1)
+            {
+                e.Graphics.DrawRectangle(new Pen(Color.Green), myPoint.X, myPoint.Y, 10, 10);
+                //e.Graphics.FillRectangle(Brushes.Green, myPoint.X, myPoint.Y, 10, 10);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1 != null && pictureBox1.Image != null)
+            {
+                System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
+                ToolTip1.SetToolTip(this.pictureBox1, "Hello");
+                //MessageBox.Show(string.Format("X: {0} Y: {1}", MousePosition.X, MousePosition.Y));
+
+                Bitmap b = new Bitmap(pictureBox1.Image);
+                myPoint = new Point(e.X, e.Y);
+
+                pictureBox1.Invalidate(); 
+                pictureBox2.Invalidate();
+            }
+        }
+    }
+}
